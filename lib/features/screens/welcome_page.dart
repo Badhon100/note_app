247101bb5c996374173b0597e04a4674599787b1
Badhon100 <badhonx102@gmail.com>
2023-08-22 15:31:31 +0600

    welcome page design done

--- lib/features/screens/welcome_page.dart
+++ lib/features/screens/welcome_page.dart
@@ -15,15 +15,35 @@ class _WelcomepageState extends State<Welcomepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: InkWell(
-          onTap: signInAnonymously,
-          child: Text(
-            "Welcome to Noteapp",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.amber[200]
+      backgroundColor: const Color(0xff252525),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SafeArea(
+          child: Center(
+            child: InkWell(
+              onTap: signInAnonymously,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Welcome to Noteapp",
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber[200]
+                    ),
+                  ),
+                  Icon(Icons.edit_calendar_rounded, size: 150, color: Colors.amber[200],),
+                  Text(
+                    "Tap anywhere to continue",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber[200]
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
